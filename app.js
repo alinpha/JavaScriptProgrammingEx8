400d2ada47ee85f0df25c06cd1eb024ce82ea0b7
Aline Vetrov <alinpha@gmail.com>
2020-02-11 11:30:12 -0400

    init

---
+++ app.js
@@ -0,0 +1,22 @@
+const myDivTag = document.getElementById('myDiv');
+myDivTag.innerHTML = 'Hello World';
+
+function clickHandler() {
+    const inputElement = document.getElementById('firstNameInput');
+    const target = document.getElementById('target-p');
+    target.innerHTML = inputElement.value;
+    console.log("btn click");
+}
+
+const button = document.getElementById('btnClickMe');
+button.addEventListener('click',clickHandler);
+
+const chk1 = document.getElementById('chkMyCheckBox');
+chk1.addEventListener('change',showCheckBoxState);
+
+function showCheckBoxState(event) {
+    //console.log(event);
+    console.log(event.target.checked);
+    
+}
+
