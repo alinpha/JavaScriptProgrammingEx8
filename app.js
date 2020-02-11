3afa7fdb63d6732be2e37e55a34ae1c61411c14b
Aline Vetrov <alinpha@gmail.com>
2020-02-11 12:02:42 -0400

    password ex added

--- app.js
+++ app.js
@@ -19,10 +19,42 @@ button.addEventListener('click',clickHandler);
 
 const chk1 = document.getElementById('chkMyCheckBox');
 chk1.addEventListener('change',showCheckBoxState);
+//chk1.addEventListener('click',showCheckBoxState);
 
 function showCheckBoxState(event) {
     //console.log(event);
     //console.log(event.target.checked);
-    
+    const target = document.getElementById('checkBoxState');
+    target.innerHTML = event.target.checked;
 }
 
+const vehicleMakes = document.getElementById('vehicleMakes');
+vehicleMakes.addEventListener('change',selectionChanged);
+//chk1.addEventListener('click',showCheckBoxState);
+
+function selectionChanged(event) {
+    //console.log(event);
+    const target = document.getElementById('selectedMake');
+
+    if (event.target.selectedIndex > 0) {
+        target.innerHTML = event.target.selectedOptions[0].text;
+    } else {
+        target.innerHTML = '';
+    }
+}
+
+
+const user = document.getElementById('user');
+const pass = document.getElementById('pass');
+const repass = document.getElementById('repass');
+
+function loginHandler() {
+
+    const target = document.getElementById('p-result');
+
+    if (pass.value == repass.value) {
+        target.innerHTML = 'passwords match';
+    } else {
+        target.innerHTML = 'passwords do not match';
+    }
+}
